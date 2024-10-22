0ed86b4588f60e286ec85375e641b2fa1824a69f
Omar Mohseni <omarmohseni.bus@gmail.com>
2024-10-22 19:01:26 +0200

    Two-Sum bruteforce stub

---
+++ c/TwoSum/src/main.c
@@ -0,0 +1,5 @@
+#include "two-sum.h"
+int main(int argc, char const *argv[])
+{
+    return 0;
+}
---
+++ c/TwoSum/src/two-sum.c
@@ -0,0 +1,7 @@
+#include "two-sum.h"
+
+#include <stdlib.h>
+
+int* two_sum_bruteforce(int* nums, int numsSize, int target, int* returnSize){
+    return NULL;
+}
